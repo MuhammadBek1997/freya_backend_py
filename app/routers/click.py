b5c644a5febd54ebdfbc6e6fecdb7a4a4f121afd
umidjonsangilov <usangilov.96@gmail.com>
2026-03-11 10:54:36 +0500

    1

--- app/routers/click.py
+++ app/routers/click.py
@@ -324,6 +324,7 @@ def pay_for_premium(
             amount=payment.amount,
             merchant_trans_id=payment.id,
         )
+        print(result.get("error_code"))
         if result.get("error_code"):
             payment.status = PaymentStatus.ERROR.value
             db.commit()
